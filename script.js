c08a366c8a6b038fc773f777e094e6f8b6831c14
cyberuv <uvsoft816@gmail.com>
2023-07-05 15:33:18 +0530

    3:32

--- script.js
+++ script.js
@@ -1,106 +1,84 @@
 
-
  // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDefTZ3hQl16jIvXbIQ3AXFvkvQWlwIpqI",
-    authDomain: "dream-web-61ad8.firebaseapp.com",
-    projectId: "dream-web-61ad8",
-    storageBucket: "dream-web-61ad8.appspot.com",
-    messagingSenderId: "40035040781",
-    appId: "1:40035040781:web:ab016bb5d72c612e325f44",
-    measurementId: "G-76X879JLY4"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-  var myName = prompt("Enter Your name");
+ const firebaseConfig = {
+  apiKey: "AIzaSyDefTZ3hQl16jIvXbIQ3AXFvkvQWlwIpqI",
+  authDomain: "dream-web-61ad8.firebaseapp.com",
+  projectId: "dream-web-61ad8",
+  storageBucket: "dream-web-61ad8.appspot.com",
+  messagingSenderId: "40035040781",
+  appId: "1:40035040781:web:ab016bb5d72c612e325f44",
+  measurementId: "G-76X879JLY4"
+};
+// Initialize Firebase app
+firebase.initializeApp(firebaseConfig);
 
-  function sendMessage(){
-    var message = document.getElementById('message').value;
+// Get references to DOM elements
+const serverInput = document.getElementById('server-input');
+const nameInput = document.getElementById('name-input');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const messageContainer = document.getElementById('message-container');
 
-    database.ref('kuchbhi').push().set({
-      "sender" : myName,
-      "message" : message,
-    })
+// Send button click event handler
+sendButton.addEventListener('click', function() {
+  const serverName = serverInput.value;
+  const name = nameInput.value;
+  const message = messageInput.value;
 
-    return false;
-  };
+  if (serverName && name && message) {
+    const serverRef = firebase.database().ref(serverName);
+    serverRef.push().set({
+      name: name,
+      message: message
+    });
 
-  database.ref('kuchbhi').on('child_added', function(snapshot){
-    var html = "";
-    html += "<li id='message-"+ snapshot.key +"'>";
-       if (snapshot.val().sender == myName) {
-         html += "<button data-id = '" + snapshot.key + "' onclick='deleteMessage(this);' >";
-         html += "Delete";
-         html += "</button>";
+    // Clear input fields
+    // nameInput.value = '';
+    messageInput.value = '';
+  }
+});
 
-         
-        }
-        html += snapshot.val().sender + ": " + snapshot.val().message;
-        html += "</li>";
-        
-        document.getElementById('messages').innerHTML += html;
-        var idName = "message-"+ snapshot.key ;
-        styleId = document.getElementById(idName);
-  });
+// Realtime listener for server messages
+serverInput.addEventListener('change', function() {
+  const serverName = serverInput.value;
 
+  // Clear message container
+  messageContainer.innerHTML = '';
 
-  function deleteMessage(self){
-    var messageId = self.getAttribute("data-id");
+  if (serverName) {
+    const serverRef = firebase.database().ref(serverName);
+    serverRef.on('child_added', function(snapshot) {
+      const message = snapshot.val();
+      displayMessage(message.name, message.message);
+    });
+  }
+});
 
-    database.ref('kuchbhi').child(messageId).remove();
-  };
+// Function to display messages
 
-  database.ref('kuchbhi').on("child_removed", function(snapshot) {
-    document.getElementById("message-"+snapshot.key).innerHTML = "This message has been removed!"
-  });
-  
-  // const messageContainer = document.getElementById('message-container');
-  // const nameInput = document.getElementById('name-input');
-  // const messageInput = document.getElementById('message-input');
-  // const sendButton = document.getElementById('send-button');
-  
-  // Function to create a new message object and save it to the database
-  // function sendMessage() {
-  //   const name = prompt("enter Your name");
-  //   const message = messageInput.value;
-  
-  //   if (name && message) {
-  //     const newMessageRef = database.ref('messages').push();
-  //     newMessageRef.set({
-  //       name: name,
-  //       message: message
-  //     });
-  
-  //     messageInput.value = '';
-  //   }
-  // }
-  
-  // // Event listener for Send button click
-  // sendButton.addEventListener('click', sendMessage);
-  
-  // // Event listener for Enter key press on message input field
-  // // messageInput.addEventListener('keydown', function(event) {
-  // //   if (event.key === 'Enter') {
-  // //     sendMessage();
-  // //   }
-  // // });
-  
-  // // Function to display new messages
-  // function displayMessage(name, message) {
-  //   const messageElement = document.createElement('p');
-  //   messageElement.innerHTML = `<strong>${name}:</strong> ${message}`;
-  //   messageContainer.appendChild(messageElement);
-  // }
-  
-  // // Function to listen for new messages in the database
-  // function listenForMessages() {
-  //   database.ref('messages').on('child_added', function(snapshot) {
-  //     const message = snapshot.val();
-  //     displayMessage(message.name, message.message);
-  //   });
-  // }
-  
-  // // Start listening for new messages
-  // listenForMessages();
-  
+// function displayMessage(name, message) {
+//   const messageElement = document.createElement('div');
+//   messageElement.innerHTML = `<strong>${name}:</strong> ${message}`;
+//   messageContainer.appendChild(messageElement);
+// }
+
+// function displayMessage(name, message) {
+//   const messageElement = document.createElement('div');
+//   const messageClass = 'message-' + name.toLowerCase().replace(/\s/g, '');
+//   messageElement.className = messageClass;
+//   messageElement.innerHTML = `<strong>${name}:</strong> ${message}`;
+//   messageContainer.appendChild(messageElement);
+// }
+
+// Function to display messages
+function displayMessage(name, message) {
+  const messageElement = document.createElement('div');
+
+  // Check if the sender's name matches your name
+  if (name === nameInput.value) {
+    messageElement.classList.add('my-message'); // Add a custom class for your messages
+  }
+
+  messageElement.innerHTML = `<strong>${name}:</strong> ${message}`;
+  messageContainer.appendChild(messageElement);
+}
